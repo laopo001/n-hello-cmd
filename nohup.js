90daecf2b3c24b0d562436bdad44f1c08f738c47
laopo001 <353272497@qq.com>
2023-12-06 16:56:39 +0800

    update

--- nohup.js
+++ nohup.js
@@ -2,6 +2,21 @@
 
 import { $, argv, echo, os, fs, cd, chalk } from 'zx';
 
-await $`nohup ${argv._} > ${argv._.join('-')}.log 2>&1 & echo $! > ${argv._.join('-')}.pid`
-console.log("pid: " + chalk.red(fs.readFileSync('run.pid').toString()))
 
+async function main() {
+
+    let path = os.homedir() + '/.n_nohup/';
+    // console.log(path)
+    await fs.ensureDir(path);
+
+    let log = `${path}${argv._.join('_')}.log`;
+    let pid = `${path}${argv._.join('_')}.pid`;
+    // console.log(log, pid)
+    // let nohupArgs = `nohup ${argv._.join(' ')} > ${log} 2>&1 & echo $! > ${pid}`
+    // console.log(nohupArgs)
+    await $`nohup ${argv._} > ${log} 2>&1 &`
+    // console.log("pid: " + chalk.red(fs.readFileSync(pid).toString()))
+
+}
+
+main()
